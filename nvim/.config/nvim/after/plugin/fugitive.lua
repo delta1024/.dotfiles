52436b48ec09da1096938a37ce5ba91da9ffc04c
Jake Stannix <jakestannix@gmail.com>
2023-05-17 21:31:58 -0600

    updated fugitive bindings

--- nvim/.config/nvim/after/plugin/fugitive.lua
+++ nvim/.config/nvim/after/plugin/fugitive.lua
@@ -1,6 +1,8 @@
 require('which-key').register({
     g = {
         name = "git",
-        s = {"<cmd>Git<cr>", 'git status'},
+        s = {"<cmd>Git<cr>", 'status'},
+        l = {"<cmd>Git log<cr>", 'log'},
+        p = {"<cmd>Git push<cr>", 'push'},
     }
 }, {prefix = "<leader>"})
