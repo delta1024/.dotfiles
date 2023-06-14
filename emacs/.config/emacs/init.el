b3d08bb5d82e1b77115ec8c4a01344fd34d05e77
Jake Stannix <jakestannix@gmail.com>
2023-06-14 13:23:42 -0600

    emacs

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,15 +1,2 @@
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(display-line-numbers 'relative))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(put 'downcase-region 'disabled nil)
-(put 'upcase-region 'disabled nil)
+
